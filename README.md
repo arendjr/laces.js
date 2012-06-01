d2f6a45c4f9b7cda7608ad0cf4350b4fd07ca015
Arend van Beelen <arendjr@gmail.com>
2012-06-01 19:35:51 +0200

    Added support for nested properties + added test suite.

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-laces.js
-========
+
+# laces.js
 
 Laces.js provides a minimal implicit state machine with automatic bindings
 
-***Rationale***
 
-Ever found yourself in the situation where it would be convenient to define variables
-in terms of other variables, and have all of them update whenever one of them changes
-value? If your answer is yes, then Laces.js might be just for you!
+## Rationale
 
-***Usage***
+Ever found yourself in the situation where it would be convenient to define
+variables in terms of other variables, and have all of them update whenever one
+of them changes value? If your answer is yes, then Laces.js might be just for
+you!
+
+## Usage
 
 Laces.js works as an implicit state machine. First, you create a state object:
 
@@ -17,9 +19,14 @@ Laces.js works as an implicit state machine. First, you create a state object:
 var state = new LacesState();
 ```
 
-The state machine is called implicit because the state is defined (implicitly) by the
-set of properties that will be assigned to it (as opposed to how a typical final
-state machine* works).
+The state machine is called implicit because the state is defined (implicitly)
+by the set of properties that will be assigned to it (as opposed to how a
+typical final state machine* works).
+
+*) See: http://en.wikipedia.org/wiki/Finite-state_machine
+
+
+### Defining properties
 
 We can define properties using the defineProperty() method:
 
@@ -29,8 +36,8 @@ state.defineProperty("lastName", "van Beelen");
 ```
 
 As you can see, nothing more than the property name and the initial value are
-required. Once the property is defined, it can be accessed using dot-notation on the
-state object:
+required. Once the property is defined, it can be accessed using dot-notation on
+the state object:
 
 ```js
 state.firstName; // "Arend"
@@ -44,8 +51,8 @@ state.defineProperty("fullName", function() { return this.firstName + " " + this
 state.fullName; // "Arend van Beelen"
 ```
 
-Still no rocket science, however. But let's watch what happens when we now modify one
-of the variables that we have used to define the fullName property:
+Still no rocket science, however. But let's watch what happens when we now
+modify one of the variables that we have used to define the fullName property:
 
 ```js
 state.firstName = "Arie";
@@ -53,10 +60,40 @@ state.firstName = "Arie";
 state.fullName; // "Arie van Beelen"
 ```
 
-As you can see, changes the value of a single property now automatically updates all
-properties that are defined in terms of that property. Needless to say, this behavior
-can also be chained, so that a property that relies on fullName also gets updated when
-firstName or lastName is modified.
+As you can see, changes the value of a single property now automatically updates
+all properties that are defined in terms of that property. Needless to say, this
+behavior can also be chained, so that a property that relies on fullName also
+gets updated when firstName or lastName is modified.
+
+
+### Nested properties
+
+It is also possible to use nested properties within your state object. Example:
+
+```js
+state.defineProperty("user", null);
+state.defineProperty("displayName", function() { return this.user && this.user.name || "Anonymous"; });
+```
+
+There are now several ways of modifying the state.user.name property, each of
+which will reflect on the displayName property:
+
+```js
+state.user = { name: "Arend" };
+
+state.displayName; // "Arend"
+
+state.user.name = "Arie";
+
+state.displayName; // "Arie"
+
+state.user = null;
+
+state.displayName; // "Anonymous"
+```
+
+
+### Custom bindings
 
 Finally, you may be interested in binding a custom callback to whenever one of those
 properties changes value:
@@ -65,18 +102,25 @@ properties changes value:
 state.bind("fullName", function(newValue, oldValue) { $(".full-name").text(newValue); });
 ```
 
+Bindings can also be chained to the defineProperty() call:
+
+```js
+state.defineProperty("fullName", function() { return this.firstName + " " + this.lastName; })
+     .bind(function(newValue, oldValue) { $(".full-name").text(newValue); });
+```
+
 Note that by default, the callback is also called initially on binding, so that you
 don't need to write your initialization code twice. If you want to disable this
 behavior however, you can pass an options object with the noInitialFire property to
 true.
 
-*) See: http://en.wikipedia.org/wiki/Finite-state_machine
 
-***Demo***
+## Demo
 
 See: https://github.com/arendjr/laces.js/tree/master/demo
 
-***Compatibility***
+
+## Compatibility
 
 - Chrome 5+
 - IE 9+
--- laces.js
+++ laces.js
@@ -16,6 +16,8 @@ function LacesState() {
 //         return value is used. If the function references any other properties
 //         of this state, implicit bindings will be created for them.
 // dependencies - (Optional) Array of explicit dependencies for this property.
+//
+// Returns a LacesProperty object for the created property.
 LacesState.prototype.defineProperty = function(propertyName, value, dependencies) {
 
     if (this._values.hasOwnProperty(propertyName)) {
@@ -26,13 +28,23 @@ LacesState.prototype.defineProperty = function(propertyName, value, dependencies
     var self = this;
     if (typeof value === "function") {
         dependencies = dependencies || [];
-        
+
+        this._functions[propertyName] = value;
+
         var source = value.toString();
-        var regExp = /this.(\w+)/g;
-        
         var match;
+
+        var regExp = /this.([\w.]+)/g;
         while ((match = regExp.exec(source)) !== null) {
-            dependencies.push(match[1]);
+            if (dependencies.indexOf(match[1]) === -1) {
+                dependencies.push(match[1]);
+            }
+        }
+        var regExp = /this.(\w+)/g;
+        while ((match = regExp.exec(source)) !== null) {
+            if (dependencies.indexOf(match[1]) === -1) {
+                dependencies.push(match[1]);
+            }
         }
 
         dependencies.forEach(function(dependency) {
@@ -41,8 +53,6 @@ LacesState.prototype.defineProperty = function(propertyName, value, dependencies
                 self._reevaluate(propertyName);
             }, { "noInitialFire": true });
         });
-
-        this._functions[propertyName] = value;
 
         value = value.apply(this);
     }
@@ -53,17 +63,19 @@ LacesState.prototype.defineProperty = function(propertyName, value, dependencies
     });
 
     this._setValue(propertyName, value);
+    
+    return new LacesProperty(this, propertyName);
 }
 
-// Binds a callback to a property so that it will be called every time the value
-// of the property changes. If the property is already defined, the callback will
-// be triggered right away too (unless the noInitialFire option is used).
+// Bind a callback to a property so that it will be called every time the value
+// of the property changes. If the property is already defined, the callback
+// will be triggered right away too (unless the noInitialFire option is used).
 //
 // propertyName - Name of the property to be watched by the callback.
 // callback - The callback to call whenever the property changes value. The
 //            callback will receive newValue and oldValue parameters.
-// options - (Optional) Options object. Set the noInitialFire property to true to
-//           disable the initial firing of the callback when the property is
+// options - (Optional) Options object. Set the noInitialFire property to true
+//           to disable the initial firing of the callback when the property is
 //           defined.
 LacesState.prototype.bind = function(propertyName, callback, options) {
 
@@ -74,8 +86,21 @@ LacesState.prototype.bind = function(propertyName, callback, options) {
     }
     this._changeListeners[propertyName].push(callback);
 
-    if (!options.noInitialFire && this._values.hasOwnProperty(propertyName)) {
-        callback.apply(this, [this._values[propertyName]]);
+    var index = propertyName.indexOf(".");
+    if (index > 0) {
+        var objectName = propertyName.substr(0, index);
+        propertyName = propertyName.substr(index + 1);
+
+        if (this._values[objectName] instanceof LacesState) {
+            var self = this;
+            this._values[objectName].bind(propertyName, function(newValue, oldValue) {
+                callback.apply(self, [newValue, oldValue]);
+            });
+        }
+    } else {
+        if (!options.noInitialFire && this._values.hasOwnProperty(propertyName)) {
+            callback.apply(this, [this._values[propertyName]]);
+        }
     }
 }
 
@@ -83,10 +108,34 @@ LacesState.prototype._setValue = function(propertyName, newValue) {
 
     var oldValue = this._values[propertyName];
     if (newValue !== oldValue) {
+        var self = this;
+        if (newValue instanceof Object) {
+            var newState;
+            if (newValue instanceof LacesState) {
+                newState = newValue;
+            } else {
+                newState = new LacesState();
+                for (var key in newValue) {
+                    newState.defineProperty(key, newValue[key]);
+                }
+            }
+
+            for (var key in this._changeListeners) {
+                if (key.substr(0, propertyName.length + 1) === propertyName + ".") {
+                    this._changeListeners[key].forEach(function(listener) {
+                        newState.bind(key.substr(propertyName.length + 1), function(newValue, oldValue) {
+                            listener.apply(self, [newValue, oldValue]);
+                        });
+                    });
+                }
+            }
+
+            newValue = newState;
+        }
+
         this._values[propertyName] = newValue;
 
         if (this._changeListeners[propertyName]) {
-            var self = this;
             this._changeListeners[propertyName].forEach(function(callback) {
                 callback.apply(self, [newValue, oldValue]);
             });
@@ -107,10 +156,42 @@ LacesState.prototype.log = function(message) {
     }
 }
 
+// Constructor.
+//
+// state - LacesState the property belongs to.
+// name - Name of the property.
+function LacesProperty(state, name) {
+
+    this._state = state;
+    this._name = name;
+}
+
+// Bind a callback to the property so that it will be called every time the
+// value of the property changes. If the property is already defined, the
+// callback will be triggered right away too (unless the noInitialFire option is
+// used).
+//
+// callback - The callback to call whenever the property changes value. The
+//            callback will receive newValue and oldValue parameters.
+// options - (Optional) Options object. Set the noInitialFire property to true
+//           to disable the initial firing of the callback when the property is
+//           defined.
+//
+// Returns the LacesProperty object.
+LacesProperty.prototype.bind = function(callback, options) {
+
+    this._state.bind(this._name, callback, options);
+    return this;
+}
+
 if (window.define !== undefined) {
-    define(function(require) { return LacesState; });
+    define(function(require) { return {
+        "LacesState": LacesState,
+        "LacesProperty": LacesProperty
+    }; });
 } else {
     window.LacesState = LacesState;
+    window.LacesProperty = LacesProperty;
 }
 
 }(this));
