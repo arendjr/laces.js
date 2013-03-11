ae72d6563181a47d1ab7f9aca0e6145eefebec35
Arend van Beelen <arendjr@gmail.com>
2013-03-11 11:03:28 +0100

    Add Grunt.js build system.

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+
+        jshint: {
+            all: ["Gruntfile.js", "laces.js", "laces.local.js", "laces.tie.js"]
+        },
+
+        uglify: {
+            laces: {
+                src: "laces.js",
+                dest: "minified/laces.min.js"
+            },
+            lacesLocal: {
+                src: "laces.local.js",
+                dest: "minified/laces.local.min.js"
+            },
+            lacesTie: {
+                src: "laces.tie.js",
+                dest: "minified/laces.tie.min.js"
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+
+    grunt.registerTask("default", ["jshint", "uglify"]);
+};
