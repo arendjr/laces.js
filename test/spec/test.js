78859a0e6d77aa7d790eeb7d56ebf27e4db2feb0
Francesco Occhipinti <francesco.occhipinti@delthametod.com>
2013-09-16 15:22:30 +0200

    new candidate test about arrays

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,33 @@
 /*global describe, it */
 'use strict';
 (function () {
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
-
+    describe('laces models', function () {
+        describe('when bound to an array', function () {
+            var array,
+                event,
+                storeEvent = function(e) {
+                    event = e;
+                };
+            beforeEach(function() {
+                array = new LacesArray([1, 2, 3]);
+                array.bind('add', storeEvent);
+                array.bind('delete', storeEvent);
+                array.bind('update', storeEvent);
+                array.bind('change', storeEvent);
+            });
+            it('supports add', function () {
+                array.push(4);
+                assert.deepEqual(event, {
+                    elements: [4],
+                    name: 'change'
+                });
+            });
+            it('supports delete', function () {
+                array.remove(2);
+                assert.deepEqual(event, {
+                    elements: [3],
+                    name: 'delete'
+                });
             });
         });
     });
